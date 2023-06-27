12860686787e08e2b5f787a15094afec549bf50d
kenneth johnston <kennethjohnston@kenneths-Mini.attlocal.net>
2023-06-27 13:47:42 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+
+
+    return <div>
+        <h1>Yo World</h1>
+        <h3>From here, we have a bundle I hope</h3>
+    </div>
+
+};
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    },
+    devServer: {
+        static: path.join(__dirname, 'public')
+    }
+
+}
