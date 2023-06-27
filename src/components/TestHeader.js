0369a52c2caebbeab30bfd05198e203d867ef7be
kenneth johnston <kennethjohnston@kenneths-Mini.attlocal.net>
2023-06-27 15:40:07 -0400

    second

--- src/components/TestHeader.js
+++ src/components/TestHeader.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const TestHeader = () => {
 
+    const toBack = async () => {
+        console.log('clicked')
+        const answer = await fetch('http://localhost:3000/clicked')
+        const parsedAnswer = await answer.json();
+        console.log(parsedAnswer)
+    }
 
-    return <h3>From here, we have maybe a component</h3>
+    return (
+        <Fragment>
+            <h3>From here, we have maybe a component</h3>
+            <button onClick={toBack}>test backend</button>
+        </Fragment>)
 
 };
 
