fc7efb41cb150a87f3cd99cbb9723416a13dcf29
kenneth johnston <kennethjohnston@kenneths-Mini.attlocal.net>
2023-06-27 14:24:15 -0400

    2nd commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Component } from 'react';
+
 import ReactDOM from "react-dom";
+import TestHeader from "./components/TestHeader";
 
 const App = () => {
 
@@ -7,6 +9,7 @@ const App = () => {
     return <div>
         <h1>Yo World</h1>
         <h3>From here, we have a bundle I hope</h3>
+        <TestHeader/>
     </div>
 
 };
---
+++ src/components/TestHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TestHeader = () => {
+
+
+    return <h3>From here, we have maybe a component</h3>
+
+};
+
+export default TestHeader;
