1ee641cfe528617b44f4a82abb3a324ba2df2c67
kenneth johnston <kennethjohnston@kenneths-Mini.attlocal.net>
2023-06-28 23:43:00 -0400

    various

--- src/components/jobs/JobsDash.js
+++ src/components/jobs/JobsDash.js
@@ -9,11 +9,21 @@ import JobDeets from './job/jobDeets/JobDeets';
 const JobsDash = () => {
     const { dash, dispatch } = useContext(DashHolder);
 
+    // useEffect(() => {
+    //     const getSpeaks = async () => {
+    //         const reply = await fetch(`http://localhost:3000/jobs_speaks/${clickedJob.id}`);
+    //         const parsedList = await reply.json();
+    //         console.log(parsedList);
+    //         // if (parsedList.engineers.length) dispatch({ type: 'engineers', list: parsedList.engineers })
+    //     }
+    //     getSpeaks();
+
+    // }, [])
+
     let displayJobs = dash.jobs.map(job => (
         <Job key={job.id} job={job} />
     ))
 
-    console.log(dash.clickedJob)
 
     return <div className={styles.jobsDashContainer}>
         <div className={styles.jobsDiv}>{displayJobs}</div>
--- src/components/jobs/job/jobDeets/JobDeets.js
+++ src/components/jobs/job/jobDeets/JobDeets.js
@@ -1,18 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import { DashHolder } from '../../../../store/object-holder';
 
 import styles from './JobDeets.module.css'
 
 const JobDeets = () => {
+    const [speaks, setSpeaks] = useState([]);
     const { dash, dispatch } = useContext(DashHolder);
     const { clickedJob } = dash;
+
+
 
     return <div className={styles.deetsContainer}>
         <div className={styles.companyDiv}>{clickedJob.company}</div>
         <div className={styles.salaryDiv}>${clickedJob.salary},000</div>
-        
-        </div>
+
+    </div>
 
 };
 
