baedea1a8f2e8cc12c296a12edf6cbb2c81aa3fc
kenneth johnston <kennethjohnston@kenneths-Mini.attlocal.net>
2023-06-29 14:59:30 -0400

    sidebar forms

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,20 +1,64 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 
 
 import styles from './Sidebar.module.css'
 import { DashHolder } from '../../store/object-holder';
 
 const Sidebar = () => {
+    const [whichForm, setWhichForm] = useState('');
+    const [formVals, setFormVals] = useState([]);
     const { dash, dispatch } = useContext(DashHolder);
 
     const closeSidebar = () => dispatch({ type: 'sidebarClick', isOpen: false })
 
+    const jobFormClicker = () => setWhichForm('job')
+    const langFormClicker = () => setWhichForm('lang');
+
+    const formInputter = (event, inputId) => {
+        console.log(event.target.value, inputId);
+        let prevVals = [...formVals];
+        prevVals[inputId] = event.target.value;
+        setFormVals(prevVals);
+    }
+
+    const sendJobUp = () => {
+        let currentVals = [...formVals];
+        let num = currentVals[2];
+        currentVals[2] = Number(num);
+        setFormVals(currentVals);
+        console.log(formVals);
+    }
+
+
+
     return <div className={styles.sidebarContainer}>
         <div className={styles.xDiv} onClick={closeSidebar}>X</div>
         <ul>
-            <li className={styles.menuOption}>ADD JOB</li>
-            <li className={styles.menuOption}>ADD LANGUAGE</li>
+            <li className={styles.menuOption} onClick={jobFormClicker}>ADD JOB</li>
+            <li className={styles.menuOption} onClick={langFormClicker}>ADD LANGUAGE</li>
         </ul>
+
+        {whichForm === 'job' && <div className={styles.formDiv}>
+            <input className={styles.input} placeholder='Title' onChange={(e) => formInputter(e, 0)} />
+            <input className={styles.input} placeholder='Company' onChange={(e) => formInputter(e, 1)} />
+            <input className={styles.input} placeholder='Salary' onChange={(e) => formInputter(e, 2)} />
+            {/*         
+            <div>
+                <check
+            </div> */}
+
+            <button className={styles.submit} onClick={sendJobUp}>Submit</button>
+
+
+        </div>
+
+        }
+
+        {whichForm === 'lang' && <div className={styles.formDiv}>
+            <input className={styles.input} placeholder='Language Name' onChange={(e) => formInputter(e, 0)} />
+            <button className={styles.submit} onClick={sendJobUp}>Submit</button>
+
+        </div>}
 
     </div>
 
